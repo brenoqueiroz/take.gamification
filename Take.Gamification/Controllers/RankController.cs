1603f70620f511bd6397fc74f00f15701d91b660
Weberson dos Santos Pereira <weberson@takenet.com.br>
2015-11-13 10:42:23 -0200

    Criado controller para exibição de rank global

---
+++ Take.Gamification/Controllers/RankController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Take.Gamification.Models;
+
+namespace Take.Gamification.Controllers
+{
+    public class RankController : Controller
+    {
+        GameContext _context = new GameContext();
+
+        // GET: Rank
+        public ActionResult Index()
+        {
+            var merits = _context.UserMerits
+                            .GroupBy(u => u.TargetUserId)
+                            .Select(g => new GroupedUserMerits()
+                             {
+                                 TargetName = g.FirstOrDefault().TargetUser.Name,
+                                 Score = g.Sum(u => u.Value)
+                             })
+                             .OrderByDescending(g => g.Score)
+                             .ToList();
+            return View(merits);
+        }
+    }
+
+    public class GroupedUserMerits
+    {
+        public string TargetName { get; set; }
+        public int Score { get; set; }
+    }
+}
